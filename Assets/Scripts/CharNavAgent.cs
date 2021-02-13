2145ceb11d5c6cdd6fd6d48711ba943a0eb2d0e7
Camilo <cgordillo@ea.com>
2021-02-14 00:29:25 +0100

    first commit

---
+++ Assets/Scripts/CharNavAgent.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Policies;
+
+[System.Serializable]
+public class CharNavAgent : Agent
+{
+	private GlobalConfig m_config;
+
+	public override void Initialize()
+	{
+		m_config = FindObjectOfType<GlobalConfig>();	
+		OnEpisodeBegin();	
+	}
+
+	public override void OnEpisodeBegin()
+	{		
+		
+	}
+
+	public void FixedUpdate() 
+	{		
+			
+	}
+
+	public override void CollectObservations(VectorSensor sensor)
+	{
+				
+	}
+	
+	public override void OnActionReceived(float[] vectorAction)
+	{	
+			
+	}
+
+	public override void Heuristic(float[] actionsOut)
+	{
+		actionsOut[0] = Input.GetAxis("Vertical");
+		actionsOut[1] = Input.GetAxis("Horizontal");
+	}
+}
---
+++ Assets/Scripts/GlobalConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+
+public class GlobalConfig : MonoBehaviour
+{
+    GameObject gameInstance;
+    public int numberOfInstances = 1;    
+    public float instancesSeparation = 70f;
+
+    void Awake()
+    {
+        numberOfInstances = Mathf.Max(numberOfInstances, 1);      // Make sure there is at least one instance
+        gameInstance = GameObject.Find("GameInstance");
+  
+        // This is were we instantiate the game areas when the game is launched
+        for (int i = 0; i<(numberOfInstances-1); i++) {
+            var gameArea = Instantiate(gameInstance, new Vector3(instancesSeparation+i*instancesSeparation, 0f, 0f), Quaternion.identity, GameObject.Find("SceneObjects").transform);
+            gameArea.name = "GameInstance" + (i+1).ToString();
+        }        
+
+    }
+}
